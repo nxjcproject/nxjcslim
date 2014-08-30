b2cb9da576e58ce70e45f42d87b45d4c38fd7ddc
decrula <decrula@gmail.com>
2014-08-30 14:18:28 +0800

    加入邵梦能耗总耗图，加入张新重点能耗监控

---
+++ NXJCSlim/NXJC.UI.Web/EnergyData/Company.aspx
@@ -0,0 +1,54 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Company.aspx.cs" Inherits="NXJC.UI.Web.EnergyData.Company" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>银川水泥公司能耗</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        
+        <div style="position:absolute; top: 5px; left: 487px;"><asp:Label ID="Label1" runat="server" Text="银川水泥公司能耗" Font-Size="XX-Large" ></asp:Label></div>
+        <div style="position:absolute;border:3px solid black;background-color:#b9b9b9;width:344px; height:216px; top: 52px; left: 12px;">
+            <br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <asp:Label ID="YFC" runat="server" Text="一分厂能耗" Font-Size="X-Large" ></asp:Label>
+            <br /><br />
+            &nbsp;总功率<asp:Label ID="YFCZGL" runat="server" Text=""></asp:Label><asp:Label ID="Label40" runat="server" Text="KWh"></asp:Label>
+            &nbsp; 总电耗<asp:Label ID="YFCZDH" runat="server" Text=""></asp:Label><asp:Label ID="Label62" runat="server" Text="KWh"></asp:Label>
+            <br />&nbsp;熟料综合电耗<asp:Label ID="YFCSLZHDH" runat="server" Text=""></asp:Label><asp:Label ID="Label76" runat="server" Text="kWh/t"></asp:Label>
+            <br />&nbsp;熟料综合煤耗<asp:Label ID="YFCSLZHMH" runat="server" Text=""></asp:Label><asp:Label ID="Label83" runat="server" Text="kgce/t"></asp:Label>
+            <br />&nbsp;熟料综合能耗<asp:Label ID="YFCSLZHNH" runat="server" Text=""></asp:Label><asp:Label ID="Label87" runat="server" Text="kgce/t"></asp:Label>
+            <br />&nbsp;水泥综合电耗<asp:Label ID="YFCSNZHDH" runat="server" Text=""></asp:Label><asp:Label ID="Label91" runat="server" Text="kWh/t"></asp:Label>
+            <br />&nbsp;水泥综合煤耗<asp:Label ID="YFCSNZHMH" runat="server" Text=""></asp:Label><asp:Label ID="Label93" runat="server" Text="kgce/t"></asp:Label>
+            <br />&nbsp;水泥综合能耗<asp:Label ID="YFCSNZHNH" runat="server" Text=""></asp:Label><asp:Label ID="Label95" runat="server" Text="kgce/t"></asp:Label>
+            <br />&nbsp;余热发电功率<asp:Label ID="YFCYRFD" runat="server" Text=""></asp:Label><asp:Label ID="Label97" runat="server" Text="MW"></asp:Label>     
+       </div>
+       <div style="position:absolute;border:3px solid black;background-color:#b9b9b9;width:344px;height:216px; top: 52px; left: 465px;">
+            <br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <asp:Label ID="ND" runat="server" Text="宁东分厂能耗" Font-Size="X-Large" ></asp:Label>
+            <br /><br />
+            &nbsp;总功率<asp:Label ID="NDZGL" runat="server" Text=""></asp:Label><asp:Label ID="Label3" runat="server" Text="KWh"></asp:Label>
+            &nbsp; 总电耗<asp:Label ID="NDZDH" runat="server" Text=""></asp:Label><asp:Label ID="Label5" runat="server" Text="KWh"></asp:Label>
+            <br />&nbsp;水泥综合电耗<asp:Label ID="NDSNZHDH" runat="server" Text=""></asp:Label><asp:Label ID="Label13" runat="server" Text="kWh/t"></asp:Label>
+            <br />&nbsp;水泥综合煤耗<asp:Label ID="NDSNZHMH" runat="server" Text=""></asp:Label><asp:Label ID="Label15" runat="server" Text="kgce/t"></asp:Label>
+            <br />&nbsp;水泥综合能耗<asp:Label ID="NDSNZHNH" runat="server" Text=""></asp:Label><asp:Label ID="Label17" runat="server" Text="kgce/t"></asp:Label>
+       </div>
+       <div style="position:absolute;border:3px solid black;background-color:#b9b9b9;width:344px;height:216px; top: 52px; left: 928px;">
+            <br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <asp:Label ID="LS" runat="server" Text="兰山分厂能耗" Font-Size="X-Large" ></asp:Label>
+            <br /><br />
+            &nbsp;总功率<asp:Label ID="LSZGL" runat="server" Text=""></asp:Label><asp:Label ID="Label2" runat="server" Text="KWh"></asp:Label>
+            &nbsp; 总电耗<asp:Label ID="LSZDH" runat="server" Text=""></asp:Label><asp:Label ID="Label4" runat="server" Text="KWh"></asp:Label>
+            <br />&nbsp;熟料综合电耗<asp:Label ID="LSSLZHDH" runat="server" Text=""></asp:Label><asp:Label ID="Label6" runat="server" Text="kWh/t"></asp:Label>
+            <br />&nbsp;熟料综合煤耗<asp:Label ID="LSSLZHMH" runat="server" Text=""></asp:Label><asp:Label ID="Label7" runat="server" Text="kgce/t"></asp:Label>
+            <br />&nbsp;熟料综合能耗<asp:Label ID="LSSLZHNH" runat="server" Text=""></asp:Label><asp:Label ID="Label8" runat="server" Text="kgce/t"></asp:Label>
+            <br />&nbsp;水泥综合电耗<asp:Label ID="LSSNZHDH" runat="server" Text=""></asp:Label><asp:Label ID="Label9" runat="server" Text="kWh/t"></asp:Label>
+            <br />&nbsp;水泥综合煤耗<asp:Label ID="LSSNZHMH" runat="server" Text=""></asp:Label><asp:Label ID="Label10" runat="server" Text="kgce/t"></asp:Label>
+            <br />&nbsp;水泥综合能耗<asp:Label ID="LSSNZHNH" runat="server" Text=""></asp:Label><asp:Label ID="Label11" runat="server" Text="kgce/t"></asp:Label>
+            <br />&nbsp;余热发电功率<asp:Label ID="LSYRFD" runat="server" Text=""></asp:Label><asp:Label ID="Label12" runat="server" Text="MW"></asp:Label>     
+       </div>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ NXJCSlim/NXJC.UI.Web/EnergyData/Company.aspx.designer.cs
@@ -0,0 +1,474 @@
+﻿//------------------------------------------------------------------------------
+// <自动生成>
+//     此代码由工具生成。
+//
+//     对此文件的更改可能会导致不正确的行为，并且如果
+//     重新生成代码，这些更改将会丢失。 
+// </自动生成>
+//------------------------------------------------------------------------------
+
+namespace NXJC.UI.Web.EnergyData {
+    
+    
+    public partial class Company {
+        
+        /// <summary>
+        /// form1 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// Label1 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+        
+        /// <summary>
+        /// YFC 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label YFC;
+        
+        /// <summary>
+        /// YFCZGL 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label YFCZGL;
+        
+        /// <summary>
+        /// Label40 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label40;
+        
+        /// <summary>
+        /// YFCZDH 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label YFCZDH;
+        
+        /// <summary>
+        /// Label62 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label62;
+        
+        /// <summary>
+        /// YFCSLZHDH 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label YFCSLZHDH;
+        
+        /// <summary>
+        /// Label76 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label76;
+        
+        /// <summary>
+        /// YFCSLZHMH 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label YFCSLZHMH;
+        
+        /// <summary>
+        /// Label83 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label83;
+        
+        /// <summary>
+        /// YFCSLZHNH 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label YFCSLZHNH;
+        
+        /// <summary>
+        /// Label87 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label87;
+        
+        /// <summary>
+        /// YFCSNZHDH 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label YFCSNZHDH;
+        
+        /// <summary>
+        /// Label91 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label91;
+        
+        /// <summary>
+        /// YFCSNZHMH 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label YFCSNZHMH;
+        
+        /// <summary>
+        /// Label93 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label93;
+        
+        /// <summary>
+        /// YFCSNZHNH 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label YFCSNZHNH;
+        
+        /// <summary>
+        /// Label95 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label95;
+        
+        /// <summary>
+        /// YFCYRFD 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label YFCYRFD;
+        
+        /// <summary>
+        /// Label97 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label97;
+        
+        /// <summary>
+        /// ND 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label ND;
+        
+        /// <summary>
+        /// NDZGL 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label NDZGL;
+        
+        /// <summary>
+        /// Label3 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label3;
+        
+        /// <summary>
+        /// NDZDH 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label NDZDH;
+        
+        /// <summary>
+        /// Label5 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label5;
+        
+        /// <summary>
+        /// NDSNZHDH 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label NDSNZHDH;
+        
+        /// <summary>
+        /// Label13 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label13;
+        
+        /// <summary>
+        /// NDSNZHMH 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label NDSNZHMH;
+        
+        /// <summary>
+        /// Label15 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label15;
+        
+        /// <summary>
+        /// NDSNZHNH 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label NDSNZHNH;
+        
+        /// <summary>
+        /// Label17 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label17;
+        
+        /// <summary>
+        /// LS 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LS;
+        
+        /// <summary>
+        /// LSZGL 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LSZGL;
+        
+        /// <summary>
+        /// Label2 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+        
+        /// <summary>
+        /// LSZDH 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LSZDH;
+        
+        /// <summary>
+        /// Label4 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label4;
+        
+        /// <summary>
+        /// LSSLZHDH 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LSSLZHDH;
+        
+        /// <summary>
+        /// Label6 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label6;
+        
+        /// <summary>
+        /// LSSLZHMH 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LSSLZHMH;
+        
+        /// <summary>
+        /// Label7 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label7;
+        
+        /// <summary>
+        /// LSSLZHNH 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LSSLZHNH;
+        
+        /// <summary>
+        /// Label8 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label8;
+        
+        /// <summary>
+        /// LSSNZHDH 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LSSNZHDH;
+        
+        /// <summary>
+        /// Label9 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label9;
+        
+        /// <summary>
+        /// LSSNZHMH 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LSSNZHMH;
+        
+        /// <summary>
+        /// Label10 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label10;
+        
+        /// <summary>
+        /// LSSNZHNH 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LSSNZHNH;
+        
+        /// <summary>
+        /// Label11 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label11;
+        
+        /// <summary>
+        /// LSYRFD 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LSYRFD;
+        
+        /// <summary>
+        /// Label12 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label12;
+    }
+}
